23f7d66183bb80e1de2fbf684f3197c8a27148cd
Spencer Brower <foxtrotalphalima@gmail.com>
2018-12-11 16:59:28 -0500

    Modified Combos / sets  to work more efficiently. Added Set.Join(), and Set.Combine() [With repetition]

--- combin/set.go
+++ combin/set.go
@@ -1,60 +1,128 @@
 package combin
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
-// Set represents a set of items that can be combined.
-type Set struct {
-	elements []interface{}
+const Delim = ","
+
+type Combo []interface{}
+
+func (c Combo) String() string {
+	if len(c) == 1 {
+		for _, v := range c {
+			return fmt.Sprint(v)
+		}
+	}
+	str := ""
+	for i, v := range c {
+		str += fmt.Sprintf("%v", v)
+		if i+1 != len(c) {
+			str += Delim
+		}
+	}
+	return str
 }
 
+// Set represents a set of items that can be combined.
+type Set []Combo
+
 // NewSet creates a new set from the given items.
-func NewSet(v ...interface{}) Set {
-	set := Set{elements: []interface{}{}}
-	for i := range v {
-		val := reflect.ValueOf(v[i])
-		if val.Kind() == reflect.Slice || val.Kind() == reflect.Array {
-			for j := 0; j < val.Len(); j++ {
-				set.elements = append(set.elements, val.Index(j))
-			}
-		} else {
-			set.elements = append(set.elements, v[i])
-		}
+func NewSet(v ...interface{}) *Set {
+	set := new(Set)
+	for _, vv := range v {
+		// val := reflect.ValueOf(v)
+		// if val.Kind() == reflect.Slice || val.Kind() == reflect.Array {
+		// 	for i := 0; i < val.Len(); i++ {
+		// 		set.Add(val.Index(i))
+		// 	}
+		// } else {
+		set.Add(vv)
+		// }
 	}
 	return set
 }
 
 // Add ads a new object to the set.
 func (s *Set) Add(v interface{}) {
-	s.elements = append(s.elements, v)
+	val := reflect.ValueOf(v)
+	if val.Kind() == reflect.Slice || val.Kind() == reflect.Array {
+		subset := make([]interface{}, val.Len())
+		for i := 0; i < val.Len(); i++ {
+			subset[i] = val.Index(i)
+		}
+		*s = append(*s, subset)
+	} else {
+		*s = append(*s, []interface{}{val})
+	}
 }
 
 // Combine returns all combinations of the given set with itself.
-func (s Set) Combine() [][2]interface{} {
-	combos := [][2]interface{}{}
-	v := s.elements
-	for i := 0; i < len(v)-1; i++ {
-		for j := i + 1; j < len(v); j++ {
-			combos = append(combos, [2]interface{}{v[i], v[j]})
+func (s *Set) Combine(repl bool) *Set {
+	if repl {
+		return s.combineR()
+	}
+	combos := []Combo{}
+	cpy := []Combo(*s)
+	len := len(*s)
+	if len == 1 {
+		return nil
+	}
+	for i := 0; i < len-1; i++ {
+		for j := i + 1; j < len; j++ {
+			combos = append(combos, []interface{}{cpy[i], cpy[j]})
 		}
 	}
-	return combos
+	set := Set(combos)
+	return &set
+}
+
+// combineR combines the set with itself (using repetition).
+func (s *Set) combineR() *Set {
+	combos := []Combo{}
+	max := len(*s)
+	cpy := []Combo(*s)
+	for i := 0; i < max; i++ {
+		for j := i; j < max; j++ {
+			item := append(cpy[i], cpy[j])
+			combos = append(combos, item)
+		}
+	}
+	set := Set(combos)
+	return &set
+}
+
+// Join adds all the entries from b to the end of s.
+func (s *Set) Join(b *Set) *Set {
+	temp := append(*s, []Combo(*b)...)
+	return &temp
 }
 
 // Size returns the number of elements in the set.
 func (s *Set) Size() int {
-	return len(s.elements)
+	return len(*s)
 }
 
 // CombineSets returns all combinations (without replacement)
 // of the items in set a with the items in set b.
-func CombineSets(a, b Set) [][]interface{} {
-	combos := make([][]interface{}, a.Size()*b.Size())
+func CombineSets(a, b Set) *Set {
+	combos := make([]Combo, a.Size()*b.Size())
 	i := 0
-	for _, aa := range a.elements {
-		for _, bb := range b.elements {
+	for _, aa := range a {
+		for _, bb := range b {
 			combos[i] = []interface{}{aa, bb}
 			i++
 		}
 	}
-	return combos
+	s := Set(combos)
+	return &s
+}
+
+func (s *Set) String() string {
+	str := ""
+	for _, v := range *s {
+		str += fmt.Sprintln(v)
+	}
+	return str
 }
